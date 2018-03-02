fd02af70d25cc5dbf10317c888062051f6b9c9b5
lucabelezal <lucabelezal@gmail.com>
2018-03-02 12:22:37 -0300

    CustomTabBar

---
+++ FacebookMessenger/Controllers/CustomTabBarController.swift
@@ -0,0 +1,34 @@
+//
+//  CustomTabBarController.swift
+//  FacebookMessenger
+//
+//  Created by Lucas Nascimento on 02/03/2018.
+//  Copyright © 2018 Lucas Nascimento. All rights reserved.
+//
+
+import UIKit
+
+class CustomTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //setup our custom view controllers
+        let layout = UICollectionViewFlowLayout()
+        let friendsController = FriendsController(collectionViewLayout: layout)
+        let recentMessagesNavController = UINavigationController(rootViewController: friendsController)
+        recentMessagesNavController.tabBarItem.title = "Recent"
+        recentMessagesNavController.tabBarItem.image = UIImage(named: "recent")
+        
+        viewControllers = [recentMessagesNavController, createDummyNavControllerWithTitle("Calls", imageName: "calls"), createDummyNavControllerWithTitle("Groups", imageName: "groups"), createDummyNavControllerWithTitle("People", imageName: "people"), createDummyNavControllerWithTitle("Settings", imageName: "settings")]
+    }
+    
+    fileprivate func createDummyNavControllerWithTitle(_ title: String, imageName: String) -> UINavigationController {
+        let viewController = UIViewController()
+        let navController = UINavigationController(rootViewController: viewController)
+        navController.tabBarItem.title = title
+        navController.tabBarItem.image = UIImage(named: imageName)
+        return navController
+    }
+    
+}
